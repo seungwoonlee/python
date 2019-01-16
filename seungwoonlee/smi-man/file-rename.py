72e0c25a9249629ef84cd007eed864647011c040
winCloud <bluecrck@gmail.com>
2019-01-17 02:32:39 +0900

    animation movie series file rename example

--- seungwoonlee/smi-man/file-rename.py
+++ seungwoonlee/smi-man/file-rename.py
@@ -1,16 +1,21 @@
 import os
-#import glob
 
-files = []
+def rename_movie_file(path, check_str):
+    for filename in os.listdir(path):
+        file_name, file_ext = os.path.splitext(filename)
+        file_ext = file_ext.lower()
+        if file_ext == '.mp4' or file_ext == '.smi' or file_ext == '.ass':
+            if check_str in filename:
+                file_name = file_name.replace(check_str, "")
+                rename_filename = file_name + file_ext
+                full_filename = os.path.join(path, filename)
+                full_rename_filename = os.path.join(path, rename_filename)
+                os.rename(full_filename, full_rename_filename)
+                #print('Renamed : ', full_filename, ' -> ', full_rename_filename)
 
-#path = "."
-#path = "C:\\Users\\seung\\python\\seungwoonlee"
-#path = "\\\\XPEnology\\ani\\슬레이어즈\\S1"
-path = '.'
-
-for filename in os.listdir(path):
-    files.append(filename)
-    print(os.path.splitext(filename))
-
-#print(path)
-#print(files)  
+if __name__ == '__main__':
+    path = "N:\\Bang Dream!\\S2"
+    #path = os.getcwd()
+    check_str = " (AT-X 1280x720 x264 AAC)"
+    #print(path)
+    rename_movie_file(path, check_str)
